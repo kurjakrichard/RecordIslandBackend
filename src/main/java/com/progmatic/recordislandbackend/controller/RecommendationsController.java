13e2141bab99c4a0d44efd13014a27ed065d90e2
Daniel Varga <varga.daniel.1985@gmail.com>
2019-07-18 14:38:39 +0200

    minor RecommendationController fix

--- src/main/java/com/progmatic/recordislandbackend/controller/RecommendationsController.java
+++ src/main/java/com/progmatic/recordislandbackend/controller/RecommendationsController.java
@@ -5,10 +5,31 @@
  */
 package com.progmatic.recordislandbackend.controller;
 
+import com.progmatic.recordislandbackend.domain.Album;
+import com.progmatic.recordislandbackend.service.RecommendationsServiceImpl;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Dano
  */
+@RestController
 public class RecommendationsController {
+    
+    RecommendationsServiceImpl recommendationsService;
+    
+    @Autowired
+    public RecommendationsController(RecommendationsServiceImpl recommendationsService) {
+        this.recommendationsService = recommendationsService;
+    }
+    
+    @GetMapping(value = {"/api/", "/api/welcome"})
+    public Set<Album> getUserRecommendations() {
+        return recommendationsService.getRecommendations();
+    }
+    
     
 }
