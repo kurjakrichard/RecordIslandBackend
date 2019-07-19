93913afa7c62b5edcecee53289ebe7894ab11e7a
mark <mark.gulacsy@gmail.com>
2019-07-19 14:28:30 +0200

    minor change in recommendation service

--- src/main/java/com/progmatic/recordislandbackend/service/RecommendationsServiceImpl.java
+++ src/main/java/com/progmatic/recordislandbackend/service/RecommendationsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.progmatic.recordislandbackend.service;
 
+import com.mysql.cj.conf.PropertyKey;
 import com.progmatic.recordislandbackend.domain.Album;
 import com.progmatic.recordislandbackend.domain.Artist;
 import com.progmatic.recordislandbackend.domain.User;
@@ -33,28 +34,29 @@ public class RecommendationsServiceImpl {
         this.lastFmService = lastFmService;
         this.discogsService = discogsService;
     }
-    
-    
+
     public Set<Album> getRecommendations() throws LastFmException {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         HashSet<Album> resultSet = new HashSet();
         List<Album> discogsAlbums = discogsService.getDiscogsPage(2019, 4);
-        
-        try {
+
         for (Album album : discogsAlbums) {
-            List<String> similarArtists = lastFmService.listSimilarArtists(album.getArtist().getName());
-            if (loggedInUser.getLikedArtists().stream().map(a -> a.getName()).anyMatch(a -> a.equals(album.getArtist().getName())) || 
-                    loggedInUser.getLikedArtists().stream().map(a -> a.getName()).anyMatch(a -> similarArtists.contains(a))) {
+            List<String> similarArtists;
+            try {
+                similarArtists = lastFmService.listSimilarArtists(album.getArtist().getName());
+            } catch (LastFmException ex) {
+                System.out.println(ex.getMessage() + album.getArtist().getName());
+                continue;
+            }
+            if (similarArtists != null && (loggedInUser.getLikedArtists().stream().map(a -> a.getName()).anyMatch(a -> a.equals(album.getArtist().getName()))
+                    || loggedInUser.getLikedArtists().stream().map(a -> a.getName()).anyMatch(a -> similarArtists.contains(a)))) {
                 resultSet.add(album);
             }
         }
-        } catch(LastFmException le) {
-           
-        };
         return resultSet;
 
     }
-    
+
     @Transactional
     @PatchMapping
     public void updateUserLikedArtists(Artist artist) {
